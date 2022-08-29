5e98b0aa9a1df92af55ec84113f50c237a83a64e
Hyuain <Hyuain97@163.com>
2022-08-29 15:12:31 +0800

    unit test for items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,11 +3,18 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.page params[:page]
     # pagination using id
     # items = Item.where("id > ?", params[:start_id]).limit(100)
-    render json: { resources: items }
+    render json: {
+      resources: items,
+      pager: {
+        page: params[:page],
+        per_page: Kaminari.config.default_per_page,
+        count: Item.count,
+      }
+    }
   end
 
   def create
-    item = Item.new amount: 1
+    item = Item.new amount: params[:amount], notes: params[:notes]
     if item.save
       render json: { resources: item }
     else
